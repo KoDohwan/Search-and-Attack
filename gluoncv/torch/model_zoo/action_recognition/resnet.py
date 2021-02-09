763b56ef03118a6866f080ccb15c174995287d1d
KoDohwan <ikodoh12@naver.com>
2021-02-09 05:30:56 +0000

    Update Config

--- gluoncv/torch/model_zoo/action_recognition/resnet.py
+++ gluoncv/torch/model_zoo/action_recognition/resnet.py
@@ -30,7 +30,7 @@ def r3d_34(cfg):
     return model
 
 def r2plus1d_18(cfg):
-    model = VideoResNet(block=BasicBlock, conv_makers=[Conv3DSimple] * 4, layers=[2, 2, 2, 2], stem=R2Plus1dStem)
+    model = VideoResNet(block=BasicBlock, conv_makers=[Conv2Plus1D] * 4, layers=[2, 2, 2, 2], stem=R2Plus1dStem)
     state_dict = load_state_dict_from_url(model_urls['r2plus1d_18'], progress=True)
     model.load_state_dict(state_dict)
     model.fc = nn.Linear(model.fc.in_features, cfg.CONFIG.DATA.NUM_CLASSES)
@@ -38,8 +38,17 @@ def r2plus1d_18(cfg):
     return model
 
 def r2plus1d_34(cfg):
-    model = VideoResNet(block=BasicBlock, conv_makers=[Conv3DSimple] * 4, layers=[3, 4, 6, 3], stem=R2Plus1dStem)
+    model = VideoResNet(block=BasicBlock, conv_makers=[Conv2Plus1D] * 4, layers=[3, 4, 6, 3], stem=R2Plus1dStem)
+    
+    
+    model.layer2[0].conv2[0] = Conv2Plus1D(128, 128, 288)
+    model.layer3[0].conv2[0] = Conv2Plus1D(256, 256, 576)
+    model.layer4[0].conv2[0] = Conv2Plus1D(512, 512, 1152)
+    model.fc = nn.Linear(model.fc.in_features, 359)
+    state_dict = torch.load('./logs/r2plus1d/pretrained.pth')
+    model.load_state_dict(state_dict)
     model.fc = nn.Linear(model.fc.in_features, cfg.CONFIG.DATA.NUM_CLASSES)
+    print('Pretrained Model Weight Loaded')
     return model
 
 if __name__ == '__main__':
